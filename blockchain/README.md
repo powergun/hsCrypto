b695cb27e24dd16467f125b8eb12c139442b87ef
Wei Ning <macgnw@gmail.com>
2019-12-31 11:00:00 +1100

    wip

--- blockchain/README.md
+++ blockchain/README.md
@@ -1,3 +1,7 @@
 # Rolling your Own Blockchain in Haskell
 
 source: <http://www.michaelburge.us/2017/08/17/rolling-your-own-blockchain.html>
+
+## Merkle tree
+
+source: <https://selfkey.org/what-is-a-merkle-tree-and-how-does-it-affect-blockchain-technology/>
---
+++ blockchain/src/Blockchain/Types.hs
@@ -0,0 +1,43 @@
+{-# LANGUAGE DeriveDataTypeable         #-}
+{-# LANGUAGE DeriveTraversable          #-}
+{-# LANGUAGE FlexibleInstances          #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE NoImplicitPrelude          #-}
+{-# LANGUAGE StandaloneDeriving         #-}
+{-# LANGUAGE TypeSynonymInstances       #-}
+
+module Blockchain.Types where
+
+import           Control.Comonad.Cofree
+import           Crypto.Hash
+import           Data.Data
+import qualified Data.Vector            as V
+import           Protolude
+
+newtype Account = Account Integer
+                  deriving (Eq, Show, Num)
+
+data Transaction = Transaction
+  { _from   :: Account
+  , _to     :: Account
+  , _amount :: Integer
+  } deriving (Eq, Show)
+
+newtype BlockF a = Block (V.Vector a)
+                   deriving ( Eq, Show, Foldable, Traversable
+                            , Functor, Semigroup, Monoid)
+
+type Block = BlockF Transaction
+
+type BlockchainHash = Digest SHA1
+
+data BlockHeader = BlockHeader
+  { _miner      :: Account
+  , _parentHash :: BlockchainHash
+  } deriving (Eq, Show)
+
+data MerkleF a = Genesis
+               | Node BlockHeader a
+               deriving (Eq, Show, Functor, Traversable, Foldable)
+
+type Blockchain = Cofree MerkleF Block
