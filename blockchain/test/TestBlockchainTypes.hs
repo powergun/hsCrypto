8b1e7a91eb21531ebe4522259865f4bb091d73a2
Wei Ning <macgnw@gmail.com>
2020-01-01 12:00:30 +1100

    blockchain

--- blockchain/test/TestBlockchainTypes.hs
+++ blockchain/test/TestBlockchainTypes.hs
@@ -3,10 +3,22 @@
 module TestBlockchainTypes where
 
 import           Blockchain.Types
-import qualified Data.Vector      as V
+import           Control.Comonad.Cofree (Cofree (..))
+import qualified Data.Vector            as V
 import           Protolude
 
-demo :: IO ()
-demo = do
-  let genesis_block = Block (V.fromList [])
-  return ()
+import           Test.Hspec
+
+demo = runTests
+
+runTests :: IO ()
+runTests = hspec $ do
+  describe "Construct a chain from scratch" $ do
+    it "Retrieve all transactions" $ do
+      let genesis_block = Block (V.fromList ([] :: [Transaction]))
+          block1 = Block (V.fromList [Transaction 0 1 1000])
+          genesis_chain = genesis_block :< Genesis  -- MerkleF
+          chain1 = block1 :< Node (BlockHeader {_miner = 0, _parentHash = undefined }) genesis_chain
+          chain2 = block1 :< Node (BlockHeader {_miner = 0, _parentHash = undefined }) chain1
+      let transactions = toList . mconcat . toList $ chain2
+      length transactions `shouldBe` 2
